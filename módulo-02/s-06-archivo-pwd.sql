b6c4ed46992df7422945ab84b51ecf82c5b2f613
Franperceb <42518428+Franperceb@users.noreply.github.com>
2023-11-22 22:25:01 -0600

    agregando ejercicios de módulo 2 respecto al archivo de password y privilegios de administración

---
+++ módulo-02/s-06-archivo-pwd.sql
@@ -0,0 +1,25 @@
+--@Autor: Jorge Francisco Pereda Ceballos
+--@Fecha creación: 13/11/2023
+--@Descripción: Ejercicio 07 -módulo 02 .Archivo de passwords.Ejecutar como administrador consulta y 
+-- creación de usuarios con privilegios de administración.
+
+prompt conectando como sysdba
+conn sys/system1 as sysdba
+
+prompt creando usuarios user01 , user02, user03
+create user user01 identified by user01;
+create user user02 identified by user02;
+create user user03 identified by user03;
+
+prompt asignando privilegios de administración a usuarios creados.
+grant sysdba, sysoper, sysbackup to user01;
+grant sysdba, sysoper, sysbackup to user02;
+grant sysdba, sysoper, sysbackup to user03;
+
+set linesize window
+col username format a20
+
+prompt mostrando datos de privilegios de administración asignado a usuarios.
+select username, sysdba, sysbackup, sysoper, account_status
+from  v$pwfile_users;
+
